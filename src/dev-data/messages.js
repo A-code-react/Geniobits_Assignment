2a6ff9195fc2811d4c936dd3986e90b38d9347cb
Ambika Shingare <amrutashingare25@gmail.com>
2023-06-20 23:40:01 +0530

    Real Estate Project Task Completed

---
+++ src/dev-data/messages.js
@@ -0,0 +1,32 @@
+export const messages = [
+  {
+    id: 1,
+    from: "Sarika J",
+    to: 3,
+    body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium aut quod molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, officiis! Fuga deserunt consequuntur iure repellendus expedita consectetur, est sequi quod qui explicabo consequatur tempora?",
+    read: true,
+    emailFrom: "kevin@gmail.com",
+    phone: +9850791913,
+    title: "Interested in The Apartment for Sale",
+  },
+  {
+    id: 2,
+    from: "Prabhakar S",
+    to: 3,
+    body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium aut quod molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, officiis! Fuga deserunt consequuntur iure repellendus expedita consectetur, est sequi quod qui explicabo consequatur tempora?",
+    read: true,
+    emailFrom: "prabhakarshingare@gmail.com",
+    phone: +9850791913,
+    title: "Interested in The House for Sell in Pune County",
+  },
+  {
+    id: 3,
+    from: "Priscilla Johnson",
+    to: 3,
+    body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium aut quod molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, officiis! Fuga deserunt consequuntur iure repellendus expedita consectetur, est sequi quod qui explicabo consequatur tempora?",
+    read: true,
+    emailFrom: "sarikashingare@gmail.com",
+    phone: +9850791913,
+    title: "Interested in The House for Sell in Pune County",
+  },
+];
